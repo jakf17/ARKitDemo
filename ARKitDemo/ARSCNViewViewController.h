7804ac53341060869249537c62b7282e5c93fcf2
yangzhibo <yangzhibo@pyyx.com>
2017-07-28 17:30:18 +0800

    first commit

---
+++ ARKitDemo/ARSCNViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  ARSCNViewViewController.h
+//  ARKit自定义实现
+//
+//  Created by jakf_17 on 2017/7/28.
+//  Copyright © 2017年 jakf_17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARSCNViewViewController : UIViewController
+
+@end
--- ARKitDemo/ViewController.m
+++ ARKitDemo/ViewController.m
@@ -1,12 +1,14 @@
 //
 //  ViewController.m
-//  ARKitDemo
+//  ARKit自定义实现
 //
-//  Created by jakf_17 on 17/7/28.
+//  Created by jakf_17 on 2017/7/28.
 //  Copyright © 2017年 jakf_17. All rights reserved.
 //
 
 #import "ViewController.h"
+
+#import "ARSCNViewViewController.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+///开启AR
+- (IBAction)startButtonClick:(id)sender {
+    
+    //创建自定义AR控制器
+    ARSCNViewViewController *vc = [[ARSCNViewViewController alloc] init];
+    //跳转
+    [self presentViewController:vc animated:YES completion:nil];
 }
 
 
